7dde38cdd2b65238e4214719e1ecb678dbbeb829
arvinaloumian1 <31023990+arvinaloumian1@users.noreply.github.com>
2017-10-11 19:15:42 -0700

    10-11-2017

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
-var grayButton = document.getElementById('grayButton');
+// var grayButton = document.getElementById('grayButton');
 
-var grayClick = function(){
-	document.body.style.backgroundColor = 'gray';
+
+$(document).ready(function(){
+  initMap();
+	// $(".readmore a").click(showHidenP);
+});
+function initMap() {
+       var uluru = {lat: 33.8479634, lng: -117.2270496};
+      //  var uluru = {lat: -25.363, lng: 131.044};
+       var map = new google.maps.Map(document.getElementById('google-map'),
+        {
+         zoom: 8,
+         center: uluru
+       });
+       var marker = new google.maps.Marker({
+  position: uluru,
+  map: map
+});
 }
+// var grayClick = function(){
+// 	document.body.style.backgroundColor = 'gray';
+// }
 
-grayButton.onclick = grayClick;
+// grayButton.onclick = grayClick;
 
-var whiteButton = document.getElementById('whiteButton');
+// var whiteButton = document.getElementById('whiteButton');
 
-var whiteClick = function(){
-	document.body.style.backgroundColor = 'black';
-	document.querySelector('h1').style.color = 'white';
-	document.querySelector('p').style.color = 'white';
-}
+// var whiteClick = function(){
+// 	document.body.style.backgroundColor = 'black';
+// 	document.querySelector('h1').style.color = 'white';
+// 	document.querySelector('p').style.color = 'white';
+// }
 
-whiteButton.onclick = whiteClick;
+// whiteButton.onclick = whiteClick;
+
+// $(window).load(function(){
+//  $('.container').find('img').each(function(){
+//   var imgClass = (this.width/this.height > 1) ? 'wide' : 'tall';
+//   $(this).addClass(imgClass);
+//  })
+// })
